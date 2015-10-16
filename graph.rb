5448805307df1bf523dfee1f602589d84ba7c98d
Victor Feijó <victor.feijoa@gmail.com>
2015-10-16 13:38:08 +0000

    Update graph.rb

--- graph.rb
+++ graph.rb
@@ -5,7 +5,7 @@ class Graph
         @edges = edges
     end
 
-    def connect a, b
+    def connect (a, b)
         create_vertice(a) unless include_vertice?(a)
         create_vertice(b) unless include_vertice?(b)
         @edges << "#{a}-#{b}"
@@ -15,11 +15,11 @@ class Graph
         @edges.each { |edge| puts edge}
     end
 
-    def include_vertice? vertice
-        @vertices.include? vertice
+    def include_vertice? (vertice)
+        @vertices.include? (vertice)
     end
 
-    def create_vertice vertice
+    def create_vertice (vertice)
         @vertices << vertice
     end
 
@@ -27,13 +27,13 @@ end
 # Just some tests...
 graph = Graph.new ['a', 'c', 'd', 'b'], ['c-d']
 graph2 = Graph.new
-graph2.connect 'y', 'u'
-graph.connect 'a', 'b'
+graph2.connect ('y', 'u')
+graph.connect ('a', 'b')
 graph.print_edges
-puts graph.include_vertice? 'b'
-puts graph.include_vertice? 'w'
-graph.connect 'w', 'a'
-graph.connect 'w', 'j'
+puts graph.include_vertice? ('b')
+puts graph.include_vertice? ('w')
+graph.connect ('w', 'a')
+graph.connect ('w', 'j')
 graph.print_edges
-puts graph.include_vertice? 'w'
+puts graph.include_vertice? ('w')
 graph2.print_edges
